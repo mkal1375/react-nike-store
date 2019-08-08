f106d64ab86ab6273a1824eb80967cee5a70ee2f
Mahdi Kalhor <mkal1375@gmail.com>
2019-08-08 23:01:42 +0430

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import Logo from "../assets/logo.svg";
+import SearchIcon from "../assets/feather/search.svg";
+import ShoppingBagIcon from "../assets/feather/shopping-bag.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header inner-wrapper">
+      <Logo className="logo" />
+      <div className="header__options">
+        <SearchIcon className="icon" />
+        <ShoppingBagIcon className="icon" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
